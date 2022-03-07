d9e2038e7c888d10a5b40e765ab9348e98ac54b2
dsfahbfhjsa22 <101074644+dsfahbfhjsa22@users.noreply.github.com>
2022-03-07 08:22:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Các cậu iu bọn tớ nhìu chứ  😘 ',
+    desc: 'ấn không bọn tớ buồn lắm đấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'chúng tớ hiểu tấm lòng của các cậu mà <333',
+    messDesc: 'chúc mùng 8/3 các cậu luôn vui vẻ',
+    btnAccept: 'tớ sẽ tỏa sáng như bông hoa',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
